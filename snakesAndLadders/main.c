1effa71c2a3173a81187635b5979278f078656f1
tahzib404 <tahzib404@gmail.com>
2024-07-13 13:47:54 +0000

    Minor fixes

--- snakesAndLadders/main.c
+++ snakesAndLadders/main.c
@@ -20,7 +20,7 @@ void initialiseBoard(int board[10][10], struct Snakes snakes[], struct Ladder la
 void printBoard(int board[10][10], int p1_pos, int p2_pos);
 int rollDice();
 void waitForEnter();
-void updatePos(int* p_pos, int dice, struct Snakes snakes[], struct Ladder ladders[]);
+int updatePos(int pos, int dice, struct Snakes snakes[], struct Ladder ladders[]);
 
 int main() {
 	int board[10][10];
@@ -62,8 +62,8 @@ int main() {
 		int dice = rollDice();
 		printf("You rolled a %d\n", dice);
 
-		if(p1_turn) updatePos(&p1_pos, dice, snakes, ladders);
-		else updatePos(&p2_pos, dice, snakes, ladders);
+		if(p1_turn) p1_pos = updatePos(p1_pos, dice, snakes, ladders);
+		else p2_pos = updatePos(p2_pos, dice, snakes, ladders);
 
 		printf("\nPOSITION:\n");
 		printf("        Player1: %d\n        Player2: %d\n\n", p1_pos, p2_pos);
@@ -177,22 +177,21 @@ void waitForEnter() {
 	} while (c != '\n');
 }
 
-void updatePos(int* p_pos, int dice, struct Snakes snakes[], struct Ladder ladders[]) {
-	*p_pos += dice;
-	if(*p_pos > 100) {
-		*p_pos = 100;
-		return;	
+int updatePos(int pos, int dice, struct Snakes snakes[], struct Ladder ladders[]) {
+	int new_pos = pos + dice;
+	if(new_pos > 100) {
+		printf("You need %d to win", 100 - pos);
+		return pos;
 	}
 	for(int i=0; i<MAX_LADDERS_AND_SNAKES; i++) {
-		if(*p_pos == snakes[i].head) {
-			*p_pos = snakes[i].tail;
-			printf("\033[31mOOPS YOU GOT EATEN BY: S%.2d\033[0m\n", *p_pos);
-			return;
+		if(new_pos == snakes[i].head) {
+			printf("\033[31mOOPS YOU GOT EATEN BY: S%.2d\033[0m\n", snakes[i].tail);
+			return snakes[i].tail;
 		}
-		if(*p_pos == ladders[i].bottom) {
-			*p_pos = ladders[i].top;
-			printf("\033[32mNICE! YOU CLIMBED: L%.2d\033[0m\n", *p_pos);
-			return;
+		if(new_pos == ladders[i].bottom) {
+			printf("\033[32mNICE! YOU CLIMBED: L%.2d\033[0m\n", ladders[i].top);
+			return ladders[i].top;
 		}
 	}
+	return new_pos;
 }
